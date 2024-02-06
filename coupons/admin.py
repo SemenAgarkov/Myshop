14781b6f75adef45d7663b691cac98d1107bc505
semen <semen.agarkov@gmail.com>
2024-02-06 16:13:07 +0300

    translate v0.2.1

--- coupons/admin.py
+++ coupons/admin.py
@@ -6,5 +6,5 @@ from .models import Coupon
 class CouponAdmin(admin.ModelAdmin):
     list_display = ['code', 'valid_from', 'valid_to',
                     'discount', 'active']
-    list_filter = ['active', 'valid_form', 'valid_to']
+    list_filter = ['active', 'valid_from', 'valid_to']
     search_fields = ['code']
--- coupons/views.py
+++ coupons/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
-from django.views.decorators.http import require_ROST
+from django.views.decorators.http import require_POST
 from .models import Coupon
 from .forms import CouponApplyForm
 
 # Create your views here.
-@require_ROST
+@require_POST
 def coupon_apply(request):
     now = timezone.now()
     form = CouponApplyForm(request.POST)
